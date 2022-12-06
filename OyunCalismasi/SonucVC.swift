3fe1d0ab998a946f2c6893213062ca815d72c928
ilkerugurkaya <kayailkerugur@gmail.com>
2022-12-06 22:08:11 +0300

    first commit

---
+++ OyunCalismasi/SonucVC.swift
@@ -0,0 +1,29 @@
+//
+//  SonucVC.swift
+//  OyunCalismasi
+//
+//  Created by İlker Kaya on 6.12.2022.
+//
+
+import UIKit
+
+class SonucVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
